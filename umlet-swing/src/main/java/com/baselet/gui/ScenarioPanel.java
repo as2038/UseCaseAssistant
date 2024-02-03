7d5d9fc9536d5a3e65d6d09bbebe6d4f9b8ae4da
as2038 <as2038@hw.ac.uk>
2024-02-03 18:37:23 +0000

    Scenario panel WIP 2

--- umlet-swing/src/main/java/com/baselet/gui/ScenarioPanel.java
+++ umlet-swing/src/main/java/com/baselet/gui/ScenarioPanel.java
@@ -11,10 +11,11 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.UIManager.LookAndFeelInfo;
-
-import com.baselet.control.constants.Constants;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class ScenarioPanel extends JPanel implements ActionListener {
 
@@ -30,17 +31,29 @@ public class ScenarioPanel extends JPanel implements ActionListener {
 	private final JFrame scenarioframe;
 	private final Vector<String> uis_technicalNames = new Vector<String>();
 
+	private final JLabel lb_prac = new JLabel("Primary actor:");
+	private final JTextField tf_prac = new JTextField();
+
+	private final JLabel lb_secac = new JLabel("Secondary actor(s):");
+	private final JTextField tf_secac = new JTextField();
+
+	private final JLabel lb_prec = new JLabel("Preconditions:");
+	private final JTextField tf_prec = new JTextField();
+
+	private final JLabel lb_postc = new JLabel("Postconditions:");
+	private final JTextField tf_postc = new JTextField();
+
+	private final JLabel lb_mainflow = new JLabel("Main flow:");
+	private final JTextArea ta_mainflow = new JTextArea(5, 5);
+	JScrollPane sp_mainflow = new JScrollPane(ta_mainflow);
+
+	private final JLabel lb_altflow = new JLabel("Alternative flows:");
+	private final JTextArea ta_altflow = new JTextArea(5, 5);
+	JScrollPane sp_altflow = new JScrollPane(ta_altflow);
+
 	private ScenarioPanel() {
 		setLayout(new GridLayout(0, 2, 4, 4));
 		setAlignmentX(Component.LEFT_ALIGNMENT);
-
-		Vector<String> uis_humanReadableNameVector = new Vector<String>();
-		LookAndFeelInfo[] lookAndFeelInfoArray = Constants.lookAndFeels.toArray(new LookAndFeelInfo[Constants.lookAndFeels.size()]);
-		for (LookAndFeelInfo info : lookAndFeelInfoArray) {
-			uis_technicalNames.add(info.getClassName());
-			uis_humanReadableNameVector.add(info.getName());
-		}
-
 		JButton button_save = new JButton("Save");
 		button_save.setActionCommand("Save");
 		button_save.addActionListener(this);
@@ -58,6 +71,19 @@ public class ScenarioPanel extends JPanel implements ActionListener {
 		button_panel.setAlignmentX(Component.LEFT_ALIGNMENT);
 
 		JPanel parent = new JPanel();
+		parent.setLayout(new BoxLayout(parent, BoxLayout.Y_AXIS));
+		parent.add(lb_prac);
+		parent.add(tf_prac);
+		parent.add(lb_secac);
+		parent.add(tf_secac);
+		parent.add(lb_prec);
+		parent.add(tf_prec);
+		parent.add(lb_postc);
+		parent.add(tf_postc);
+		parent.add(lb_mainflow);
+		parent.add(sp_mainflow);
+		parent.add(lb_altflow);
+		parent.add(sp_altflow);
 		parent.add(button_panel);
 
 		scenarioframe = new JFrame("Scenario");
@@ -82,9 +108,13 @@ public class ScenarioPanel extends JPanel implements ActionListener {
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+	public void actionPerformed(ActionEvent ae) {
+		if (ae.getActionCommand().equals("Save")) {
+			System.out.println("Saved scenario...");
+		}
+		if (ae.getActionCommand().equals("Close")) {
+			hideScenarioPanel();
+		}
 	}
 
 }
